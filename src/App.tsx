ce3b9d1d07d5661872570985f0602b1e287d406b
mrussellpaige <mrussellpaige@gmail.com>
2026-04-13 19:25:51 -0400

    Update SVG arrow path to point at face area

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
-<svg ...>
-  <path d="M28% 30% ..." /> <!-- Adjusted path for heroArrow -->
-</svg>
+import React from 'react';
+
+const App = () => {
+  return (
+    <svg width="200" height="200">
+      <path d="M20 20 C 100 0, 120 80, 115 100" fill="transparent" stroke="black" />
+      <polygon points="110,95 125,110 100,115" fill="black" />
+    </svg>
+  );
+};
+
+export default App;
