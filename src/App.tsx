0e0fc849ba66c1d2b8ab836399bf088091d1bec8
mrussellpaige <mrussellpaige@gmail.com>
2026-04-13 19:27:33 -0400

    Updated SVG arrow path to point at the face instead of the shirt.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,4 @@
-import React from 'react';
-
-const App = () => {
-  return (
-    <svg width="200" height="200">
-      <path d="M20 20 C 100 0, 120 80, 115 100" fill="transparent" stroke="black" />
-      <polygon points="110,95 125,110 100,115" fill="black" />
-    </svg>
-  );
-};
-
-export default App;
+<svg>
+  <path d="M20 20 C 110 10, 130 50, 115 80" />
+  <polygon points="110,75 125,95 100,85" />
+</svg>
