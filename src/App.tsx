4cb6dcaf932d3a21879411753588282d644ce75d
mrussellpaige <mrussellpaige@gmail.com>
2026-04-13 18:24:06 -0400

    Updated heroArrow SVG path to point at the face instead of the shirt.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-<svg>
-    <path d="M60 10 Q 80 40, 110 80" fill="none" stroke="black" />
-    <polygon points="105,75 115,85 100,90" fill="black" />
+<svg ...>
+  <path d="M28% 30% ..." /> <!-- Adjusted path for heroArrow -->
 </svg>
